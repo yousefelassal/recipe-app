58ceda50452fb63120ef4ed23f3c50171bf8e76f
Yousef <yousefsameh24@gmail.com>
2023-07-30 00:53:08 +0300

    handle querying

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,36 @@
-import { createClient } from 'next-sanity'
+import { 
+  createClient,
+  groq
+} from 'next-sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
-export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-})
+export async function getRecipes() {
+  const client = createClient({
+    apiVersion,
+    dataset,
+    projectId,
+    useCdn,
+  })
+
+  client.fetch(
+    groq`*[_type == "recipe" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": mainImage.asset->url,
+      "chef": chef->{
+        name,
+      },
+      "ingredients": ingredients[]{
+        "name": ingredient->name,
+        unit,
+        wholeNumber,
+        fraction,
+      },
+      instructions,
+    }`
+  )
+
+}
