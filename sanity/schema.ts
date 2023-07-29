a772fc88626abc2045d232b149fda3a2b3c63484
Yousef <yousefsameh24@gmail.com>
2023-07-29 21:40:36 +0300

    update schemas: add recipe schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,8 +1,12 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import chef from './schemas/chef'
+import ingredient from './schemas/ingredient'
+import recipe from './schemas/recipe'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     chef,
+    ingredient,
+    recipe,
   ],
 }
