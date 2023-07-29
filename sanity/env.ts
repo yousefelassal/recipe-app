2b8df7a2fc2fbab699a32278fba0fdddde2e6808
Yousef <yousefsameh24@gmail.com>
2023-07-29 20:54:04 +0300

    fix studio error
    .env is not read for some reason

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-29'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "production",
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "gz6x2j30",
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
