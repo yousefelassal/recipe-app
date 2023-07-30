4c1e854202be2a7d938c44cd73d8abf93862f3e0
Yousef <yousefsameh24@gmail.com>
2023-07-30 16:07:01 +0300

    create client functions to fetch pages

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -4,6 +4,7 @@ import {
 } from 'next-sanity'
 
 import { Recipe } from '@/types/Recipe'
+import { Page } from '@/types/Page'
 import clientConfig from '@/sanity/config/client-config'
 
 export async function getRecipes(): Promise<Recipe[]> {
@@ -48,6 +49,30 @@ export async function getRecipe(slug:string): Promise<Recipe>{
       },
       instructions,
       likes,
+    }`,
+    { slug }
+  )
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+    }`
+  )
+}
+
+export async function getPage(slug:string): Promise<Page>{
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+      content,
     }`,
     { slug }
   )
