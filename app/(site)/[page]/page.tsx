b93c660ffea0944d0bc45d30b902623b0d50b980
Yousef <yousefsameh24@gmail.com>
2023-07-30 16:41:47 +0300

    update page style

--- app/(site)/[page]/page.tsx
+++ app/(site)/[page]/page.tsx
@@ -14,7 +14,12 @@ export default async function Page({ params }: Props){
     return (
         <div className="pt-20 px-36 flex flex-col gap-8">
             <header>
-                <h1 className="text-2xl">{page.title}</h1>
+                <h1 
+                    className="text-5xl bg-gradient-to-r from-orange-600 via-red-500 to-purple-600 
+                        bg-clip-text text-transparent drop-shadow font-bold w-auto"
+                >
+                    {page.title}
+                </h1>
             </header>
             <div className="text-gray-200 text-lg flex flex-col gap-4">
                 <PortableText value={page.content} />
