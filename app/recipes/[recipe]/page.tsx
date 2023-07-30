e077c797499e4fc2fee726889eb8fd110424fd30
Yousef <yousefsameh24@gmail.com>
2023-07-30 15:14:20 +0300

    update recipe page

--- app/recipes/[recipe]/page.tsx
+++ app/recipes/[recipe]/page.tsx
@@ -1,4 +1,6 @@
 import { getRecipe } from '@/sanity/lib/client';
+import { PortableText } from '@portabletext/react'
+import Image from 'next/image';
 
 type Props = {
     params: {
@@ -11,8 +13,18 @@ export default async function Recipe({params}: Props){
     const recipe = await getRecipe(slug);
 
     return (
-        <div className="pt-20 p-8 flexs flex-col">
-            <h1 className="text-2xl">{recipe.name}</h1>
+        <div className="pt-20 px-36 flex flex-col gap-8">
+            <header>
+                <h1 className="text-2xl">{recipe.name}</h1>
+            </header>
+            <div className="text-gray-200 justify-between text-lg flex">
+                <PortableText value={recipe.instructions} />
+                <div className="w-96 h-80 rounded-lg overflow-hidden relative">
+                {recipe.image && (
+                    <Image src={recipe.image} fill alt={"photo of " + recipe.name} className="object-cover"/>
+                )}
+                </div>
+            </div>
         </div>
     )
 }
