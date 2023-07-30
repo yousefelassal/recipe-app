1255e5b218f520c023bde557d6d29a39cad69a6b
Yousef <yousefsameh24@gmail.com>
2023-07-30 13:08:46 +0300

    update home page style

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,21 @@ import Link from 'next/link';
 import { getRecipes } from '@/sanity/lib/client';
 
 export default async function Home() {
-  const data = await getRecipes();
-  console.log(data);
+  const recipes = await getRecipes();
 
   return (
-    <div className="min-h-screen p-4 pt-12">
-      recipes go here
+    <div className="gap-8 flex flex-wrap p-8 pt-16">
+      {recipes.map((recipe) => (
+        <div key={recipe._id} className="w-52 h-48 rounded-lg border border-gray-600 flex flex-col gap-2 items-center">
+          <div className="w-full h-44 relative bg-gray-700">
+          {recipe.image && (
+              <Image src={recipe.image} fill alt={"photo of " + recipe.name}/>
+              )}
+          </div>
+          {recipe.name}
+        </div>
+        )
+      )}
     </div>
   )
 }
