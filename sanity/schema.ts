8cce4f13df6d928744fb8e1799bd3ef84a921b8d
Yousef <yousefsameh24@gmail.com>
2023-07-30 16:06:33 +0300

    link page schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,11 +2,13 @@ import { type SchemaTypeDefinition } from 'sanity'
 import chef from './schemas/chef'
 import ingredient from './schemas/ingredient'
 import recipe from './schemas/recipe'
+import page from './schemas/page'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     chef,
     ingredient,
     recipe,
+    page,
   ],
 }
