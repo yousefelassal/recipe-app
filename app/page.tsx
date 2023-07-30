7e740675f7d4c5db151367b419776af215d0b844
Yousef <yousefsameh24@gmail.com>
2023-07-30 13:48:34 +0300

    update style

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,15 @@ import { getRecipes } from '@/sanity/lib/client';
 
 export default async function Home() {
   const recipes = await getRecipes();
+  console.log(recipes);
 
   return (
     <div className="gap-8 flex flex-wrap p-8 pt-16">
       {recipes.map((recipe) => (
-        <div key={recipe._id} className="w-52 h-48 rounded-lg border border-gray-600 flex flex-col gap-2 items-center">
-          <div className="w-full h-44 relative bg-gray-700">
+        <div key={recipe._id} className="w-52 h-48 rounded-lg border border-gray-600 flex flex-col gap-2 items-center overflow-hidden">
+          <div className="w-full h-44 relative bg-gray-700 overflow-hidden">
           {recipe.image && (
-              <Image src={recipe.image} fill alt={"photo of " + recipe.name}/>
+              <Image src={recipe.image} fill alt={"photo of " + recipe.name} />
               )}
           </div>
           {recipe.name}
