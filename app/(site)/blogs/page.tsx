f94d00dcd1e06f2496ff1a305f150e79e6493bd4
Yousef <yousefsameh24@gmail.com>
2023-07-31 19:07:30 +0300

    update blogs page: update margin

--- app/(site)/blogs/page.tsx
+++ app/(site)/blogs/page.tsx
@@ -17,7 +17,7 @@ export default async function Blog() {
             Get Inspired with Our Blog Posts
             </p>
           </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <div className="mx-auto mt-4 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-6 sm:mt-8 sm:pt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {blogs.map((blog) => (
             <Link key={blog._id} href={`/blogs/${blog.slug}`}>
             <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
