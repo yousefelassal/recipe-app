3a3a39fa2092f12986b711b5a711ec30190f9d67
Yousef <yousefsameh24@gmail.com>
2023-08-01 17:11:25 +0300

    update landing page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
                                     News
                                 </span>
                                 <p className='flex items-center'>
-                                    Read the launch post from here
+                                    Read the launch post
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="group-hover:translate-x-1 transition-transform w-5 h-5">
                                         <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                                     </svg>
