43cbcc5f5c90f60b06a2392a1b8356c3f8e414d0
Yousef <yousefsameh24@gmail.com>
2023-08-02 17:40:37 +0300

    revalidate on demand

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,16 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { revalidateTag } from 'next/cache'
+import { revalidatePath } from 'next/cache'
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
+
+const SANITY_WEBHOOK_SECRET = "07775000"
  
-// e.g a webhook to `your-website.com/api/revalidate?tag=collection&secret=<token>`
 export async function GET(request: NextRequest, res: any) {
   // Check for secret to confirm this is a valid request
-  if (
-    request.nextUrl.searchParams.get('secret') !== "07775000"
-  ) {
-    return res.status(401).json({ message: 'Invalid token' })
+  const signature:any = request.headers.get(SIGNATURE_HEADER_NAME)
+  const isValid = isValidSignature(JSON.stringify(request.body), signature, SANITY_WEBHOOK_SECRET)
+
+  console.log(`==== Is the weebhook request valid: ${isValid}`)
+
+  if (!isValid) {
+    return res.status(401).json({ message: 'Invalid signature' })
   }
- 
-  const tag: any = request.nextUrl.searchParams.get('tag')
-  revalidateTag(tag)
+  
+  const mainPath = request.body._type === 'recipe' ? '/recipes'
+   : request.body._type === 'page' ? '/pages' 
+   : request.body._type === 'blog' ? '/blogs'
+   : ''
+  
+  const pathToRevalidate = request.body._type === 'recipe' ? `/recipes/${request.body.slug.current}` 
+   : request.body._type === 'page' ? `/pages/${request.body.slug.current}`
+   : request.body._type === 'blog' ? `/blogs/${request.body.slug.current}`
+   : ''
+  
+  console.log(`==== Revalidating path: ${mainPath}`)
+
+  revalidatePath(mainPath)
+  revalidatePath(pathToRevalidate)
+  
   return NextResponse.json({ revalidated: true, now: Date.now() })
 }
