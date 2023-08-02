d639f2b5d0f20caf7560afb1aacebf7898480db1
Yousef <yousefsameh24@gmail.com>
2023-08-02 16:02:39 +0300

    fix route error

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { revalidateTag } from 'next/cache'
  
 // e.g a webhook to `your-website.com/api/revalidate?tag=collection&secret=<token>`
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, res: any) {
   // Check for secret to confirm this is a valid request
   if (
     request.nextUrl.searchParams.get('secret') !== "07775000"
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest) {
     return res.status(401).json({ message: 'Invalid token' })
   }
  
-  const tag = request.nextUrl.searchParams.get('tag')
+  const tag: any = request.nextUrl.searchParams.get('tag')
   revalidateTag(tag)
   return NextResponse.json({ revalidated: true, now: Date.now() })
 }
