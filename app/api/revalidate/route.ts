15b3473880be9bda421a1112e1e12de938ddfbad
Yousef <yousefsameh24@gmail.com>
2023-08-02 15:47:34 +0300

    handle on demand revalidation

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,31 @@
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
+
+const SANITY_WEBHOOK_SECRET = "07775000";
+
+export default async function handler(req: any, res: any) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME];
+  const isValid = isValidSignature(JSON.stringify(req.body), signature, SANITY_WEBHOOK_SECRET);
+
+  console.log(`===== Is the webhook request valid? ${isValid}`);
+
+  // Validate signature
+  if (!isValid) {
+    res.status(401).json({ success: false, message: 'Invalid signature' });
+    return;
+  }
+
+  try {
+    const pathToRevalidate = req.body.slug.current;
+
+    console.log(`===== Revalidating: ${pathToRevalidate}`);
+
+    await res.revalidate(pathToRevalidate);
+
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // Could not revalidate. The stale page will continue to be shown until
+    // this issue is fixed.
+    return res.status(500).send('Error while revalidating');
+  }
+}
+
