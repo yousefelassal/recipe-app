057728c59f463c2aab4a15f9c2b368377c96fe30
Yousef <yousefsameh24@gmail.com>
2023-08-02 17:43:47 +0300

    fix type error

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -5,30 +5,33 @@ import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 const SANITY_WEBHOOK_SECRET = "07775000"
  
 export async function GET(request: NextRequest, res: any) {
-  // Check for secret to confirm this is a valid request
-  const signature:any = request.headers.get(SIGNATURE_HEADER_NAME)
-  const isValid = isValidSignature(JSON.stringify(request.body), signature, SANITY_WEBHOOK_SECRET)
+    // Check for secret to confirm this is a valid request
+    const signature:any = request.headers.get(SIGNATURE_HEADER_NAME)
+    const isValid = isValidSignature(JSON.stringify(request.body), signature, SANITY_WEBHOOK_SECRET)
 
-  console.log(`==== Is the weebhook request valid: ${isValid}`)
+    console.log(`==== Is the webhook request valid: ${isValid}`)
 
-  if (!isValid) {
-    return res.status(401).json({ message: 'Invalid signature' })
-  }
-  
-  const mainPath = request.body._type === 'recipe' ? '/recipes'
-   : request.body._type === 'page' ? '/pages' 
-   : request.body._type === 'blog' ? '/blogs'
-   : ''
-  
-  const pathToRevalidate = request.body._type === 'recipe' ? `/recipes/${request.body.slug.current}` 
-   : request.body._type === 'page' ? `/pages/${request.body.slug.current}`
-   : request.body._type === 'blog' ? `/blogs/${request.body.slug.current}`
-   : ''
-  
-  console.log(`==== Revalidating path: ${mainPath}`)
+    if (!isValid) {
+        return res.status(401).json({ message: 'Invalid signature' })
+    }
+    
+    const requestBody = await request.text()
+    const parsedBody = JSON.parse(requestBody)
 
-  revalidatePath(mainPath)
-  revalidatePath(pathToRevalidate)
-  
-  return NextResponse.json({ revalidated: true, now: Date.now() })
+    const mainPath = parsedBody._type === 'recipe' ? '/recipes'
+     : parsedBody._type === 'page' ? '/pages' 
+     : parsedBody._type === 'blog' ? '/blogs'
+     : ''
+    
+    const pathToRevalidate = parsedBody._type === 'recipe' ? `/recipes/${parsedBody.slug?.current}` 
+     : parsedBody._type === 'page' ? `/pages/${parsedBody.slug?.current}`
+     : parsedBody._type === 'blog' ? `/blogs/${parsedBody.slug?.current}`
+     : ''
+    
+    console.log(`==== Revalidating path: ${mainPath}`)
+
+    revalidatePath(mainPath)
+    revalidatePath(pathToRevalidate)
+    
+    return NextResponse.json({ revalidated: true, now: Date.now() })
 }
