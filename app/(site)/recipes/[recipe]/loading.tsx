3f757d9544845da1771aa2a85296adec9c2e84ae
Yousef <yousefsameh24@gmail.com>
2023-08-03 20:13:32 +0300

    update recipe page loading state

--- app/(site)/recipes/[recipe]/loading.tsx
+++ app/(site)/recipes/[recipe]/loading.tsx
@@ -1,12 +1,13 @@
 export default function loading() {
   return (
-    <section className="grid place-items-center w-full h-full">
+    <section className="grid place-items-center w-full h-screen">
         <div
           className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
           role="status">
           <span
             className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-            >Loading...
+          >
+            Loading...
           </span>
         </div>
     </section>
