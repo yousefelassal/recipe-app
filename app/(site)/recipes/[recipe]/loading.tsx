6705151abb5b17b390585e78f50dcae8133aa624
Yousef <yousefsameh24@gmail.com>
2023-08-03 15:20:07 +0300

    recipe page: add loading state

---
+++ app/(site)/recipes/[recipe]/loading.tsx
@@ -0,0 +1,22 @@
+export default function loading() {
+  return (
+    <section className="mt-24 mx-auto max-w-screen-xl overflow-x-hidden pb-12 px-4 flex flex-col gap-6 items-center lg:gap-14 lg:flex-row lg:justify-center md:px-8 animate-pulse">
+        <div className="flex flex-col gap-2">
+            <h1 className="text-gray-300 text-center font-bold text-4xl xl:text-5xl w-24 h-2"></h1>
+            <div className="w-56 h-56 bg-gray-300 rounded-full"></div>
+        </div>
+        <div className="leading-relaxed mt-3">
+                <p className="w-64 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-64 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+            </div>
+    </section>
+  )
+}
