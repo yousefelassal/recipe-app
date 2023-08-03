2e162d47cfbf121e701861619cc7c72f793db80e
Yousef <yousefsameh24@gmail.com>
2023-08-03 20:09:49 +0300

    update recipe page loading state

--- app/(site)/recipes/[recipe]/loading.tsx
+++ app/(site)/recipes/[recipe]/loading.tsx
@@ -1,22 +1,14 @@
 export default function loading() {
   return (
-    <section className="mt-24 mx-auto max-w-screen-xl overflow-x-hidden pb-12 px-4 flex flex-col gap-6 items-center lg:gap-14 lg:flex-row lg:justify-center md:px-8 animate-pulse">
-        <div className="flex flex-col gap-2">
-            <h1 className="text-gray-300 text-center font-bold text-4xl xl:text-5xl w-24 h-2"></h1>
-            <div className="w-56 h-56 bg-gray-300 rounded-full"></div>
+    <section className="grid place-items-center w-full h-full">
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status">
+          <span
+            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...
+          </span>
         </div>
-        <div className="leading-relaxed mt-3">
-                <p className="w-64 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-64 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-                <p className="w-72 h-2 mt-4 bg-gray-200 rounded-lg"></p>
-            </div>
     </section>
   )
 }
