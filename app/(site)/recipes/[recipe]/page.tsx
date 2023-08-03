d47c5b02045d95d297ba67f69c417d1d23faf4fd
Yousef <yousefsameh24@gmail.com>
2023-08-03 15:09:26 +0300

    update recipe page

--- app/(site)/recipes/[recipe]/page.tsx
+++ app/(site)/recipes/[recipe]/page.tsx
@@ -13,26 +13,26 @@ export default async function Recipe({params}: Props){
     const recipe = await getRecipe(slug);
 
     return (
-        <div className="pt-20 px-36 flex flex-col gap-8 relative overflow-hidden min-h-screen">
-            <header className="flex justify-center">
-                <h1 className="text-5xl bg-gradient-to-r from-purple-600 via-red-500 to-orange-600 
-                        bg-clip-text text-transparent drop-shadow font-bold w-auto">
-                    {recipe.name}
+        <section className="mt-24 mx-auto max-w-screen-xl overflow-x-hidden pb-12 px-4 flex flex-col gap-6 items-center lg:gap-14 lg:flex-row lg:justify-center md:px-8">
+            <div className="relative">
+                <h1 className="text-indigo-900 text-center font-bold text-4xl xl:text-5xl">
+                {recipe.name}
                 </h1>
-            </header>
-            <div className="text-gray-800 justify-between text-lg flex flex-wrap">
-                <div className="relative">
-                    {recipe.image && (
-                        <Image src={recipe.image} width={400} height={400} alt={"photo of " + recipe.name} className="object-cover"/>
-                    )}
-                    <div className="absolute top-0 -z-10">
-                        <Image src="/blob.svg" width={500} height={500} alt="blob" className="opacity-50" />
-                    </div>
+                {recipe.image && (
+                    <Image src={recipe.image} width={400} height={400} alt={"photo of " + recipe.name} className="object-cover"/>
+                )}
+                <div className="absolute top-0 -z-10">
+                    <Image src="/blob.svg" width={500} height={500} alt="blob" className="opacity-50" />
                 </div>
+            </div>
+            <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
+            <div className="space-y-4 flex-1 sm:text-center lg:text-left">
                 <div className="flex flex-col gap-2" id="handwritten">
                     <PortableText value={recipe.instructions} />
                 </div>
             </div>
-        </div>
+            </div>
+        </section>
     )
 }
+
