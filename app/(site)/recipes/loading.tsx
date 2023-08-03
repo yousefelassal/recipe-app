184ab7d2574235f25a5ea7f5b7fbc2098e0a3127
Yousef <yousefsameh24@gmail.com>
2023-08-03 12:31:19 +0300

    recipes page: add loading state

---
+++ app/(site)/recipes/loading.tsx
@@ -0,0 +1,70 @@
+export default function loading() {
+  return (
+    <section className="pt-12">
+      <div className="container px-4 sm:px-6 lg:px-12 py-10 mx-auto animate-pulse">
+          <h1 className="w-48 h-2 bg-gray-200 rounded-lg "></h1>
+
+          <p className="w-64 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+          <p className="w-64 h-2 mt-4 bg-gray-200 rounded-lg sm:w-80 "></p>
+
+          <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+
+              <div className="w-full ">
+                  <div className="w-full h-64 bg-gray-300 rounded-lg "></div>
+                  
+                  <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                  <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+              </div>
+          </div>
+      </div>
+  </section>
+  )
+}
