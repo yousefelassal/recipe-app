7452abbc949806b7a5a075da8cf5c0f9883ac142
Yousef <yousefsameh24@gmail.com>
2023-08-05 00:03:59 +0300

    link logo cloud

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link';
 import Testimonials from '@/components/Testimonials';
 import Features from '@/components/Features';
+import LogoCloud from '@/components/LogoCloud';
 import { getClients } from '@/sanity/lib/client';
 
 export default async function Home() {
@@ -52,6 +53,7 @@ export default async function Home() {
                     </div>
                 </section>
             </div>
+            <LogoCloud />
             <div className="relative">
                 <div className="absolute inset-0 max-w-xs mx-auto h-44 blur-[118px]" style={{ background: "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)" }}></div>
                 <Features />
