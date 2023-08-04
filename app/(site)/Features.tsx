fe6dd339136d3904612d98d00470683327137b1f
Yousef <yousefsameh24@gmail.com>
2023-08-04 15:34:43 +0300

    create features section & link card

---
+++ app/(site)/Features.tsx
@@ -0,0 +1,57 @@
+import Card from '@/components/Card';
+
+export default function Features() {
+
+    const features = [
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                </svg>,
+            title: "Easy search and filtering",
+            desc: "Find the perfect recipe for you with our powerful search and filtering tools."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+                </svg>,
+            title: "A massive library of recipes",
+            desc: "We have over 100,000 recipes to choose from, covering all cuisines and dietary restrictions."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                </svg>,
+            title: "Step-by-step instructions",
+            desc: "Our recipes include clear and concise step-by-step instructions, so you can cook like a pro."
+        },
+    ]
+
+    return (
+        <section className="py-14 overflow-hidden">
+            <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+                <div className="relative max-w-2xl mx-auto sm:text-center">
+                    <div className="relative z-10">
+                        <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                            The Ultimate Cooking Experience
+                        </h3>
+                        <p className="mt-3">
+                            Our recipes web app has everything you need to find, cook, and enjoy your favorite recipes.
+                        </p>
+                    </div>
+                </div>
+                <div className="relative mt-12">
+                    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                        {
+                            features.map((item, idx) => (
+                                <Card key={idx} icon={item.icon} title={item.title} desc={item.desc} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
