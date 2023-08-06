8c21c35ab35e1d2044052d10fdefd13469ccc251
Yousef <yousefsameh24@gmail.com>
2023-08-06 16:35:46 +0300

    update nav, link navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,44 +1,12 @@
-import Link from 'next/link'
 import { getPages } from '@/sanity/lib/client'
-import Image from 'next/image'
+import Navbar from '@/components/Navbar'
 
 export default async function Nav(){
   const pages = await getPages();
 
     return(
-        <nav className="flex fixed top-0 items-center justify-between px-12 pt-4 pb-1 w-screen z-50 backdrop-blur-sm">
-          <Link href="/">
-            <Image src="/logo.png" width={125} height={75} alt="logo" priority />
-          </Link>
-          <div className="flex gap-4">
-            <Link
-              href="/recipes"
-              className="hover:underline hover:underline-offset-2 text-gray-600 hover:text-gray-900"
-            >
-              Recipes
-            </Link>
-            <Link
-              href="/blogs"
-              className="hover:underline hover:underline-offset-2 text-gray-600 hover:text-gray-900"
-            >
-              Blogs
-            </Link>
-            <Link
-              href="/contact"
-              className="hover:underline hover:underline-offset-2 text-gray-600 hover:text-gray-900"
-            >
-              Contact
-            </Link>
-            {pages.map((page) => (
-              <Link 
-                href={`/${page.slug}`}
-                key={page._id}
-                className="hover:underline hover:underline-offset-2 text-gray-600 hover:text-gray-900"
-              >
-                {page.title}
-              </Link>
-              ))}
-          </div>
-        </nav>
+        <>
+          <Navbar pages={pages} />
+        </>
     )
 }
